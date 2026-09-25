db87b6459baef258886e062b562a6eb6e7cf114f
NickFrias <nicolas.friasreid@code.berlin>
2026-09-25 21:32:47 +0200

    FEATURE ♠️  Add login and register links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <p class="mt-4 text-gray-700">
             PassportPlease is a tool for manufacturers to publish Digital Product Passports. Manage your passports, products and materials, with all the data in one place.
         </p>
-    <a class="inline-block mt-6 px-4 py-2 bg-blue-600 text-white rounded" href="/login">
+    <a class="inline-block mt-6 px-4 py-2 bg-blue-600 text-white rounded" href="{{ route('login') }}">
         Log in
     </a>
 </x-site-layout>
